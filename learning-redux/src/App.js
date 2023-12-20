f1dbfd2e07f7653cac193ac4a64f56d22d8a204d
aravii-27 <aravindhshanmugam27@gmail.com>
2023-12-20 21:56:47 +0530

    Route setup done for Home and Users page

--- learning-redux/src/App.js
+++ learning-redux/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+import usersReducer from "./store/reducer/user";
 
-function App() {
+const App = () => {
+  const store = configureStore({
+    reducer: {
+      users: usersReducer,
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/users" element={<Users/>} />
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
