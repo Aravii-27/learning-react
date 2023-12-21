5c4a27558a7ca04211a6812ff94ab462770941bc
aravii-27 <aravindhshanmugam27@gmail.com>
2023-12-21 17:20:32 +0530

    Added add user in home page

--- learning-redux/src/pages/Home.js
+++ learning-redux/src/pages/Home.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [user, setUser] = useState(
+    {
+      id: "",
+      name: "",
+      password: "",
+    }
+  );
 
-export default Home
+  const inputHandler = (event) => {
+    const { name, value } = event.target;
+    console.log(name,value)
+    setUser((prevItems) => {
+      return {
+        ...prevItems,
+        [name]: value,
+      };
+    });
+  };
+  return (
+    
+      <div>
+        <h1>Add User</h1>
+        <input
+          name="id"
+          placeholder="enter id"
+          onChange={inputHandler}
+          value={user.id}
+        />
+        <input
+          name="name"
+          placeholder="enter name"
+          onChange={inputHandler}
+          value={user.name}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="enter password"
+          onChange={inputHandler}
+          value={user.password}
+        />
+        <button>Add</button>
+      </div>
+  
+  );
+};
+
+export default Home;
