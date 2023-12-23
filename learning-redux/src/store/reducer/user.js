79252d10576b892c1631e399bf4a8b819ed3966f
aravii-27 <aravindhshanmugam27@gmail.com>
2023-12-23 10:29:47 +0530

    display users list

--- learning-redux/src/store/reducer/user.js
+++ learning-redux/src/store/reducer/user.js
@@ -1,14 +1,12 @@
-const  initialState= {
-  users: [],
-};
+const  initialState= []
+ 
+
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_USER":
-      return {
-        ...state,
-        users: [...state.users, action.user],
-      };
+      return [...state,action.user]
+      
     default:
       return state;
   }
